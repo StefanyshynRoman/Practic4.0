46324a78aceeb7e8ccef066d9e8dde2cdf668681
roman <romanpz051@gmail.com>
2023-05-23 19:37:07 +0200

    3.01

--- src/main/java/com/shpp/rstefanyshyn/App.java
+++ src/main/java/com/shpp/rstefanyshyn/App.java
@@ -4,6 +4,9 @@ import org.apache.activemq.util.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 
 public class App implements Constant {
 /*
@@ -18,42 +21,85 @@ RPS - 6127.075546841493, Bach size - 2000
  RPS - 6394.2707334228535, Bach size - 2000
  RPS - 6709.1580006709155, Bach size - 10000
  RPS - 6572.461386789352, Bach size - 10000
+ 1_000_000
+ RPS - 6715.420620370557, Bach size - 500
+RPS - 7447.0144918902015, Bach size - 1000
+  RPS - 6981.631327976095, Bach size - 2000
+  RPS - 7448.179292571931, Bach size - 10000
  */
 
+    static BDConnection bdConnection = new BDConnection();
     private static final Logger logger = LoggerFactory.getLogger(App.class);
+    static StopWatch stopWatch = new StopWatch();
+    static ProductGenerator productGenerator;
+    //static ProductGenerator1 productGenerator;
+
+    public static void main(String[] args) throws Exception {
+
+        // BDConnection bdConnection = new BDConnection();
+        connectToBD();
+        readDLL();
+        genHelpTable();
+        generateProduct();
+        generatFinishTable();
+        findProduct();
+        disconnect();
+    }
+
+    private static void disconnect() throws SQLException {
+        bdConnection.disconnect();
+    }
+
+    private static void findProduct() {
+        stopWatch.restart();
+        ProductFind productFind = new ProductFind(bdConnection.getConnection());
+        productFind.find();
+        stopWatch.stop();
+        logger.info("Find  products type , seconds {} ", (stopWatch.taken()) / THOUSAND);
+    }
+
+    private static void generatFinishTable() throws SQLException {
+        TableFinish tableFinish = new TableFinish(bdConnection.getConnection());
+        logger.warn("store_address: {} , valid_products: {}, type_products: {}",
+                tableFinish.getRowCountFromStore(), tableFinish.getRowCountFromProduct(), productGenerator.getRowCountFromType());
+
+        stopWatch.restart();
+        tableFinish.generateFinishTable();
+        stopWatch.stop();
+        logger.info("Finish table create {} ", (stopWatch.taken()) / THOUSAND);
+    }
 
 
-    public static void main(String[] args) throws Exception  {
-            StopWatch stopWatch=new StopWatch();
-            BDConnection bdConnection = new BDConnection();
-            bdConnection.disconnect();
-            bdConnection.connect();
-            ExecuteSqlFile executeSqlFile=new ExecuteSqlFile(bdConnection.getConnection());
-            executeSqlFile.readDLL();
-            stopWatch.restart();
-            TableGen tableGen=new TableGen( bdConnection.getConnection());
+    private static void connectToBD() throws SQLException {
+        bdConnection.disconnect();
+        bdConnection.connect();
+    }
 
-            tableGen.reader(ADDRESS_VALUES,FILE_STORE);
-            tableGen.reader(TYPE_VALUES,FILE_TYPE);
-            stopWatch.stop();
-            logger.info("Time (second) of create table store and type {} ", (stopWatch.taken()) /THOUSAND);
-            ProductGenerator productGenerator=
-                    new ProductGenerator(bdConnection.getConnection());
-            productGenerator.createProductStream();
-            TableFinish tableFinish=new TableFinish( bdConnection.getConnection());
-            logger.warn("store_address: {} , valid_products: {}, type_products: {}",
-                    tableFinish.getRowCountFromStore(), tableFinish.getRowCountFromProduct(), productGenerator.getRowCountFromType());
-            stopWatch.restart();
-            tableFinish.generateFinishTable();
-            stopWatch.stop();
-            logger.info("Finish table create {} ", (stopWatch.taken()) /THOUSAND);
-            stopWatch.restart();
-            ProductFind productFind=new ProductFind(bdConnection.getConnection());
-            productFind.find();
-            stopWatch.stop();
-            logger.info("Find  products type , seconds {} ", (stopWatch.taken()) /THOUSAND);
+    private static void readDLL() {
+        ExecuteSqlFile executeSqlFile = new ExecuteSqlFile(bdConnection.getConnection());
+        executeSqlFile.readDLL();
+    }
 
-            bdConnection.disconnect();
+    private static void genHelpTable() throws SQLException, IOException {
+        stopWatch.restart();
+        TableGen tableGen = new TableGen(bdConnection.getConnection());
+
+        tableGen.reader(ADDRESS_VALUES, FILE_STORE);
+        tableGen.reader(TYPE_VALUES, FILE_TYPE);
+        stopWatch.stop();
+        logger.info("Time (second) of create table store and type {} ", (stopWatch.taken()) / THOUSAND);
+
+    }
+
+            private static void generateProduct() {
+                productGenerator=
+                        new ProductGenerator(bdConnection.getConnection());
+                productGenerator.createProductStream();
         }
+//    private static void generateProduct() {
+//        productGenerator =
+//                new ProductGenerator1(bdConnection.getConnection());
+//        productGenerator.createProductStream();
+//    }
 
 }
---
+++ src/main/java/com/shpp/rstefanyshyn/ProductGenerator1.java
@@ -0,0 +1,142 @@
+package com.shpp.rstefanyshyn;
+
+
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.apache.activemq.util.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class ProductGenerator1 implements Constant {
+    private static final Logger logger = LoggerFactory.getLogger(ProductGenerator1.class);
+    Connection connection;
+    static Random random = new Random();
+    AtomicInteger counter = new AtomicInteger();
+    ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    Validator validator = validatorFactory.getValidator();
+
+    public ProductGenerator1(Connection connection) {
+        this.connection = connection;
+
+    }
+
+
+    public void createProductStream() {
+        int batchSize = Integer.parseInt(BATH_SIZE);
+        int numberOfProducts = Integer.parseInt(NUMBER_PRODUCTS);
+
+        try {
+            String insertQuery = "INSERT INTO products (type_id, name) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.restart();
+
+            int availableProcessors = Runtime.getRuntime().availableProcessors();
+            logger.error("dsfafsfd {} ",availableProcessors);
+            ExecutorService executorService = Executors.newFixedThreadPool(availableProcessors);
+            List<Future<Void>> futures = new ArrayList<>();
+
+            for (int i = 1; i <= numberOfProducts; i++) {
+                int finalI = i;
+                futures.add(executorService.submit(() -> {
+                    int productType;
+                    try {
+                        productType = generateType();
+                    } catch (SQLException e) {
+                        logger.error("Generate type error", e);
+                        return null;
+                    }
+
+                    String productName = generateRandomProductName();
+                    Product product = new Product(productType, productName);
+                    Set<ConstraintViolation<Product>> violations = validator.validate(product);
+
+                    if (violations.isEmpty()) {
+                        try {
+                            preparedStatement.setInt(1, productType);
+                            preparedStatement.setString(2, productName);
+                            preparedStatement.addBatch();
+
+                            if (finalI % batchSize == 0) {
+                                preparedStatement.executeBatch();
+                                preparedStatement.clearBatch();
+                            }
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        counter.getAndIncrement();
+                    }
+
+                    return null;
+                }));
+            }
+
+            // Wait for all tasks to complete
+            for (Future<Void> future : futures) {
+                try {
+                    future.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    logger.error("Task execution error", e);
+                }
+            }
+
+            preparedStatement.executeBatch();
+            stopWatch.stop();
+
+            logger.info("Generation products: {}", numberOfProducts);
+            logger.info("Generation products is over: seconds - {}", stopWatch.taken() / THOUSAND);
+            logger.warn("RPS - {}, Batch size - {}", 1000.0 * numberOfProducts / stopWatch.taken(), BATH_SIZE);
+            logger.warn("Product invalid: {}", counter);
+
+            preparedStatement.close();
+
+            executorService.shutdown();
+        } catch (SQLException e) {
+            logger.error("Product generator error: {}", e.getMessage());
+        }
+    }
+    public int getRowCountFromType() throws SQLException {
+        String countQuery = "SELECT COUNT(*) AS row_count FROM type";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(countQuery);
+            if (resultSet.next()) {
+                return resultSet.getInt("row_count");
+            }
+        }
+        return 0;
+    }
+
+
+    public int generateType() throws SQLException {
+        return random.nextInt(getRowCountFromType()) + 1;
+
+    }
+
+    public  String generateRandomProductName() {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int randomLimit = 5;
+        Random randomProductName = new Random();
+        int targetStringLength = randomProductName.nextInt(randomLimit) + randomLimit;
+
+        return randomProductName.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+}
